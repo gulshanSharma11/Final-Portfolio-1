7ea6a01f3f53548651e612854fb0a24c8834a717
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2023-08-13 22:06:10 +0530

    Update resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,7 +30,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">Skills</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="projects.html">Projects</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="education.html">Education</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="research.html">Contact</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="/assets/images/home/Gulshan Sharma.pdf" download>Resume</a></li>
    
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
